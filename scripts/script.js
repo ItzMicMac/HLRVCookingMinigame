56a766599970261d63225405b6cf10539e593f7c
McKinley Edwards <edwardsmack10@gmail.com>
2024-11-18 16:09:21 -0500

    basic UI created added all necessary files

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+const HEAT_MAX = 100; // maximum temp
+const HEAT_MIN = 0; // minimum temp
+const HEAT_STEP = 5; // amount per click the temp changes
+const HEAT_CHANGE_MIN_INTERVAL_S = 5; // seconds
+const HEAT_CHANGE_MAX_INTERVAL_S = 15; // seconds
+const COOK_TIME_S = 120; // seconds
+const WHITE_QUALITY_THRESHOLD = .6; // minimum ratio of time spent while on correct temp to produce white
+const CLOUDY_QUALITY_THRESHOLD = .8; // minimum ratio of time spent while on correct temp to produce cloudy
+const CLEAR_QUALITY_THRESHOLD = .9; // minimum ratio of time spent while on correct temp to produce clear
+
+function init(){
+    
+}
