58e9116412d36ad73c890f9cb8ae4c42300141dd
McKinley Edwards <edwardsmack10@gmail.com>
2024-11-25 14:52:11 -0500

    Added Const for Explosion Threshold

--- scripts/script.js
+++ scripts/script.js
@@ -4,9 +4,13 @@ const HEAT_STEP = 5; // amount per click the temp changes
 const HEAT_CHANGE_MIN_INTERVAL_S = 5; // seconds
 const HEAT_CHANGE_MAX_INTERVAL_S = 15; // seconds
 const COOK_TIME_S = 120; // seconds
+const EXPLOSION_THRESHOLD = .5; // minimum ratio of time spent while on incorrect temp to cause explosion
 const WHITE_QUALITY_THRESHOLD = .6; // minimum ratio of time spent while on correct temp to produce white
 const CLOUDY_QUALITY_THRESHOLD = .8; // minimum ratio of time spent while on correct temp to produce cloudy
 const CLEAR_QUALITY_THRESHOLD = .9; // minimum ratio of time spent while on correct temp to produce clear
+
+var currentTemp = 0;
+var timeElapsed = 0;
 
 function init(){
     
